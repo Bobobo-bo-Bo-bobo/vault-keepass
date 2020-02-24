d42084545061783c34c541505949d5d5b53266b1
Andreas Maus <maus@badphish.ypbind.de>
2020-02-24 18:55:16 +0100

    LIST does only work on directories - see https://www.vaultproject.io/api-docs/secret/kv/kv-v1/ so we try GET to see if it is a file instead.

--- src/vault-keepass/cmd_list.go
+++ src/vault-keepass/cmd_list.go
@@ -23,11 +23,29 @@ func cmdList(cfg *Configuration, args []string) error {
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		log.WithFields(log.Fields{
-			"http_status":         resp.StatusCode,
-			"http_status_message": resp.Status,
-		}).Error("Invalid HTTP status received")
-		return fmt.Errorf("Listing of Vault keys failed")
+		if resp.StatusCode == http.StatusNotFound {
+			// Note: LIST does only work on directories - see https://www.vaultproject.io/api-docs/secret/kv/kv-v1/
+			//       so we try GET to see if it is a file instead
+
+			resp, err = httpRequest(cfg, cfg.VaultURL+cfg.VaultPath, "GET", nil, nil)
+			if err != nil {
+				return err
+			}
+
+			if resp.StatusCode != http.StatusOK {
+				log.WithFields(log.Fields{
+					"http_status":         resp.StatusCode,
+					"http_status_message": resp.Status,
+				}).Error("Invalid HTTP status received")
+				return fmt.Errorf("Listing of Vault keys failed")
+			}
+		} else {
+			log.WithFields(log.Fields{
+				"http_status":         resp.StatusCode,
+				"http_status_message": resp.Status,
+			}).Error("Invalid HTTP status received")
+			return fmt.Errorf("Listing of Vault keys failed")
+		}
 	}
 
 	err = json.Unmarshal(resp.Content, &res)
